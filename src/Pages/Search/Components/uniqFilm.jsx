eae997875f61e86638135e1a0b950e04ee0fc925
cyrilletual <cyrille.tual@gmail.com>
2023-05-01 23:05:16 +0200

    film react api

---
+++ src/Pages/Search/Components/uniqFilm.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { API_URL_IMG } from '../../../data/const'
+import styles from "./uniqfilm.module.css"
+
+function UniqFilm({film}) {
+
+    // on va chercher la liste des compagnies de prod
+
+    let idFilm = film.id
+
+    console.log (idFilm)
+
+    function getListCompagnies (id){
+       // fetch(`${BASE_URL}/movie/${idFilm}?api_key=${API_KEY}`)
+           // .then(res => res.json())
+           // .then(res => console.log(res.production_companies))
+        //     .catch(err => console.log(err))
+    }
+
+  
+
+
+    console.log (film)
+
+  return (
+        <>
+            <h2>{film.original_title}</h2>
+            {film.poster_path && 
+                <img className={styles.pic} src={(API_URL_IMG)+(film.poster_path)} alt="" /> 
+            }
+            <p>Release : {(new Date(film.release_date).toLocaleDateString())}</p>
+            <p>{film.overview}</p> 
+        </>
+    
+  )
+}
+
+export default UniqFilm
--- src/Pages/Search/Search.jsx
+++ src/Pages/Search/Search.jsx
@@ -1,13 +1,15 @@
 
 import React, { useEffect, useState } from 'react';
 import {API_KEY, BASE_URL} from '../../data/const'
+import UniqFilm from './Components/UniqFilm';
+import styles from './search.module.css'
 
 
 const Search = () => {
 
     const [searchInput, setSearchInput] = React.useState("");  // input 
     const [filmsSearch, setFilmsSeach] = useState([]); // tableau des films proposé
-    const [filmsSelected, setFilmsSelected] = useState([]); // le film particulier selectionné 
+    const [filmsSelected, setFilmsSelected] = useState({}); // le film particulier selectionné 
 
     const handleChange = (e) => {
         setSearchInput (e.target.value);
@@ -21,30 +23,34 @@ const Search = () => {
 
     const getDetail = (e) => {
         console.log(e.target.dataset.id);
-        const uri = `${BASE_URL}/movie/${e.target.dataset.id}?api_key=${API_KEY}`
+        const uri = `${BASE_URL}/movie/${e.target.dataset.id}?api_key=${API_KEY}&language=en-US`
+       // https://api.themoviedb.org/3/movie/74944?api_key=9c6de8c116e01800dc9c56fe546028f4&language=en-US
         const encodedURI = encodeURI(uri);
         fetch(encodedURI)
         .then(res => res.json())
-        .then ((datas) => (setFilmsSelected((datas.results))))
+        .then ((datas) => (setFilmsSelected(datas)))
         .catch(err => console.log(err));
-        
-        console.log (filmsSelected)
     }
 
     return (
-        <div>
+        <div class={styles.detail}>
             <h1> Vous cherchez quelque chose? </h1>
-            <div>
+            <div className={styles.searchInput}>
                 <input type="text" name="search-input" id="search-input" value={searchInput} onChange={handleChange}/>
             </div>
-            <div id="wrapper">
+            <div  className={styles.wrapper}>
                 <ul id = "list">
                     {filmsSearch.map((film => 
                         <li key ={film.id}>
                             <a data-id ={film.id} href="#" onClick={getDetail}>{film.original_title} </a>
                         </li> ))
                     }
-                </ul>     
+                    
+                </ul>
+                <div className={styles.half} >
+                    <UniqFilm film={filmsSelected}/>  
+                </div>  
+                 
             </div>   
         </div>
     );
